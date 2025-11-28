5d79014472da4ce89effeaacc695b80e2119b6df
Nasimms <Nasim.ms90@gmail.com>
2025-11-28 12:28:09 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an Employee object and set name values.
+        Employee emp = new Employee()
+        {
+            FirstName = "Nasim",
+            LastName = "Misrabi"
+        };
+
+        // Polymorphism: Interface variable referring to an Employee object.
+        IQuitable quitter = emp;
+
+        // Calling the Quit() method using the interface reference.
+        quitter.Quit();
+
+        Console.ReadLine(); // Pause so user can see output.
+    }
+}
